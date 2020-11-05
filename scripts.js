3f40a8676bc581ba6a9f1a10df3d4b7a9105ed5c
s3prototype <liuqahs15@gmail.com>
2020-11-04 19:18:15 -0500

    Got it working. Playing with CSS.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,87 @@
 $(function(){
     //GET/READ
-    $('get-button').on('click', function(){
-       console.log('TEST'); 
+    $('#get-products').on('click', function(){
+       //console.log("TEST");
+       $.ajax({
+           url: '/products',
+           contentType: 'application/json',
+           success: function (response){
+                //console.log(response);
+                var tbodyEl = $('tbody');
+                tbodyEl.html('');
+
+                response.products.forEach(function(product){
+                    tbodyEl.append('\
+                        <tr>\
+                            <td class="id">' + product.id + '</td>\
+                            <td><input type="text" class="name" value="' +
+                            product.name + '"></td>\
+                            <td><button class="update-button">UPDATE/PUT\
+                            </button>\
+                            <button class="delete-button">DELETE</button>\
+                            </td>\
+                        </tr>\
+                    ');
+                });
+
+           }
+
+
+       });
     });
+
+    //POST/CREATE
+    $('#create-form').on('submit', function(event){
+        event.preventDefault();
+        let createInput = $('#create-input');
+
+        $.ajax({
+            url: '/products',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ name: createInput.val() }),
+            success: function(response){
+                console.log("inputworked!");
+                createInput.val('');
+                $('#get-products').click();
+            }
+
+        });
+           
+    });
+
+    //PUT/UPDATE
+    $('table').on('click', '.update-button', function(){
+        let rowEl = $(this).closest('tr');
+        let id = rowEl.find('.id').text();
+        let newName = rowEl.find('.name').val();
+
+        $.ajax({
+            url: '/products/' + id,
+            method: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify({newName: newName}),
+            success: function(response){
+                console.log(this.url);
+                $('get-products').click();
+            }
+        });
+    });
+
+    //DELETE
+    $('table').on('click', '.delete-button', function(){
+        let rowEl = $(this).closest('tr');
+        let id = rowEl.find('.id').text();
+
+        $.ajax({
+            url: '/products/' + id,
+            method: 'DELETE',
+            contentType: 'application/json',
+            success: function(response){
+                console.log(this.url);
+                $('#get-products').click();
+            }
+        });
+    });
+
 });
--- server.js
+++ server.js
@@ -19,6 +19,53 @@ var port = process.env.PORT || 8084
 
 
 app.use(express.static(__dirname));
+app.use(bodyparser.json());
+
+app.get('/products', function(req, res){
+    res.send({products : products});
+});
+
+app.post('/products', function(req, res){
+    let productName = req.body.name;
+    currentId++;
+
+    products.push({
+        id: currentId,
+        name: productName
+    });
+
+    res.send('Successfully created product!');
+
+});
+
+app.put('/products/:id', function(req, res){
+    let id = req.params.id;
+    let found = false;
+    let newName = req.body.newName;
+
+    products.forEach(function(product, index){
+        if(!found && product.id === Number(id)){
+            product.name = newName;
+        }
+    });
+
+    res.send('successfully updated products!');
+
+});
+
+app.delete('/products/:id', function(req, res){
+    let id = req.params.id;
+    let found = false;
+
+    products.forEach(function(individualProduct, index){
+        if(!found && individualProduct.id === Number(id)){
+            products.splice(index, 1);
+            found = true;
+        }
+    });
+
+    res.send('successfully deleted product');
+});
 
 app.listen(port, function(){
     console.log(`Server listening on: ${port}`);
