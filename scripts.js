05674c75a023cf3a2c1aeed729bc06a27cf49afd
s3prototype <liuqahs15@gmail.com>
2020-11-08 17:18:24 -0500

    New updates. Usernames (mostly) work. Should be relatively functioning code.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,40 @@
 $(function(){
 
+    $('#name-input').val('');//reset name
     let letterArray = ["a", "b", "c", "x", "y", "z"];
-    let userName = 'ANON-';
-    userName += Math.random().toString(36).substring(7);
+    let localName = '';
+    let userID = Math.random().toString(36).substring(7);
+    const anonName = 'ANON-' + userID;
+    let nameOnServer = anonName;
+
+    let foundAClientSideError = false;
+    //Set of clientside errors:
+    const ANON_NAME_ERR = "You cannot create your own anon name!";
+
+    console.log(userID);
 
     let timeStamp = 0;
     let messageInput = $('#message-input');
     let chatTbody = $('#chatbox-tbody');
     let chatTable = $('#chat-table');
-    let localName = userName;
     let seenArray = [false, false, false];
-    
+    let nameToRemove = null;
+
     function checkForMessages(){
         $.ajax({
             url: '/messages',
             method: 'GET',
             contentType: 'application/json',
-            success: function (response){ 
-                let localID = parseInt(response.message_id);
-                if(response.name != localName && !seenArray[localID]){
-                    console.log("ResponseName: " + response.name);
-                    addMessageToChat(response);
-                    if(isNaN(localID)) console.log("LOCAL ID BROKE!");                    
-                    seenArray[localID] = true;
+            success: function (response){
+                const incMessage = response.message;
+                if(incMessage.message_id){
+                    let localID = parseInt(incMessage.message_id);
+                    if(incMessage.name != nameOnServer && !seenArray[localID]){
+                        console.log("Response: " + incMessage.message_data);
+                        addMessageToChat(response);
+                        if(isNaN(localID)) console.log("LOCAL ID BROKE!");                    
+                        seenArray[localID] = true;
+                    }
                 }
             }
         });
@@ -30,23 +42,90 @@ $(function(){
 
     setInterval(checkForMessages, 500);
 
-    function addMessageToChat(chatMessage){
-            //Now we insert the data into the table.
-            let chatHTML = '\
+    function addMessageToChat(response){
+
+        const chatMessage = response.message;
+        if(chatMessage && chatMessage.name == localName){
+            //If we were allowed to keep the name
+            nameOnServer = chatMessage.name;
+            //And we no longer have a name to remove.
+            nameToRemove = null;
+            messageInput.val(''); //clear the input field
+        } else {
+            //if we weren't allowed the name, clear it.
+            $('#name-input').val('');
+        }
+
+        //Now we insert the data into the table.
+        //console.log("YOU KNOW WHO IT IS");
+        let chatHTML = '\
                 <tr class="chat-row"><td class="chatter-timestamp">['+
                 chatMessage.timestamp +']</td>\
                 <td class="chatter-name">' + chatMessage.name +
                 ': </td><td class="chatter-message">' +
                 chatMessage.message_data + '</td></tr>';
-                           
-            localName = chatMessage.name;
-            chatTbody.append(chatHTML.toString());
-            chatTable.scrollTop(chatTable.height() * chatMessage.message_id);                
-            messageInput.val('');
-            let messageID = parseInt(chatMessage.message_id);
-            console.log(messageID);
-            seenArray[messageID] = true;        
-    }    
+             
+        chatTbody.append(chatHTML.toString());
+        chatTable.scrollTop(chatTable.height() * chatMessage.message_id);                        
+        let messageID = parseInt(chatMessage.message_id);
+        console.log("Above message's ID: "+messageID);
+        seenArray[messageID] = true;                 
+    }
+
+    function checkNameForErrors(){
+        let errorDected = false;
+        if(localName.substr(0, 5) == 'ANON-' && localName != anonName){
+            localName = anonName;
+            clientSideChatError(ANON_NAME_ERR);
+            errorDected = true;
+        }
+        return errorDected;
+    }
+
+    function clientSideChatError(errorCode){        
+        let errorMessage = {
+            "message" : {
+                "name": "Error",
+                "message_id": -1,
+                "timestamp": timeStamp,
+                "message_data": errorCode,
+                "user_id" : userID
+            }
+        };
+        addMessageToChat(errorMessage);
+    }
+
+    function setName(){        
+        localName = $('#name-input').val();
+
+        let nameIsChanged = false;
+
+        let nameHasErrors = checkNameForErrors();
+        if(!nameHasErrors){
+
+            /*if there's a client-side error,
+            try to catch it*/
+            if(localName == ''){
+                localName = anonName;           
+            }
+    
+            if (localName != nameOnServer){                        
+                //If the name has been changed,
+                //tell the server            
+    
+                nameToRemove = {"name" : nameOnServer, "id" : userID};
+                nameIsChanged = true;
+            } else {
+                //otherwise there is no name to remove
+                nameToRemove = null;
+            }
+        } else {
+            foundAClientSideError = true;
+        }
+
+
+        return(nameIsChanged);
+    }
 
     //GET/READ
     $('#get-products').on('click', function(){
@@ -81,35 +160,47 @@ $(function(){
 
     $('#name-form').on('submit', function(event){
         event.preventDefault();
-        userName = $('#name-input').val();
-        localName = userName; 
+        //setName();
     });
 
     //PUT/UPDATE
     //message input
     $('#input-form').on('submit', function(event){
         event.preventDefault();
-        //let userName = $('#user-name');
         timeStamp = new Date().toLocaleTimeString();
         messageInput = $('#message-input');
         chatTbody = $('#chatbox-tbody');
         chatTable = $('#chat-table');
 
-        let tempMessage = {
-            "name" : userName,
-            "message_id" : 0,
-            "timestamp" : timeStamp,
-            "message_data" : messageInput.val()
-        }
+        setName();
+    
+    if(foundAClientSideError){
+        foundAClientSideError = false;
+        return;
+    }
 
-        $.ajax({
-            url: '/messages',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(tempMessage),
-            success: addMessageToChat
-        });        
-    });
+        let tempMessage =
+        {
+                "message" :
+                    {
+                        "name" : localName,
+                        "message_id" : 0,
+                        "timestamp" : timeStamp,
+                        "message_data" : messageInput.val(),
+                        "user_id": userID
+                    },
+                "name" : nameToRemove
+                            
+        };//tempMessage
+
+            $.ajax({
+                url: '/messages',
+                method: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(tempMessage, null, 2),
+                success: addMessageToChat
+            });       
+        });
 
     //DELETE
     $('table').on('click', '.delete-button', function(){
--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const e = require('express');
 
 var app = express();
 
-let rawData = fs.readFileSync('messages.json');
-let messages = JSON.parse(rawData);
-let mostRecentMessageID = 0
-let maxUnwritten = 5;
-let isItTimeToWrite = false;
+//let rawData = fs.readFileSync('messages.json');
 
-var messageSentEmitter = new events.EventEmitter();
-let messageCameFromHere = false;
-messageSentEmitter.on('message-sent', function(messageData){
-    res.send(messages[messages.length]);
-});
+let defaultMessage = {
+    "name": "SERVER",
+    "message_id": 0,
+    "timestamp": "00:00:00 AM",
+    "message_data": " ",
+    "user_id" : "SERVER_MESSAGE"
+};
+
+let messages = [{}];//was: JSON.parse(rawData);
+
+let takenNames = [{}];
 
 var products = [
     {
@@ -33,8 +35,60 @@ let emptyIds = [];
 
 var currentId = 2;
 
-var port = process.env.PORT || 8084
+var port = process.env.PORT || 8090
 
+function isNameAvailable(thisMessage){
+    let nameAvailable = true;
+    for (i = 0; nameAvailable == true && i < takenNames.length; i++){
+        if (takenNames[i] && takenNames[i].name == thisMessage.name){
+            /*if the name is already taken, then check if the userID
+            matches. If so, then the name belongs to this user.*/
+            if (takenNames[i].id == thisMessage.user_id){
+                /*if the name is already in use, and it's
+                being used by this user, then we're done.*/
+                break;
+            } else {
+                nameAvailable = false;
+            }//else
+        }//if (takenNames ...
+    }//for
+
+    return nameAvailable;
+}
+
+function makeNameAvailable(nameToRelease){
+    let nameWasFound = false;
+    for(i = 0; !nameWasFound && i < takenNames.length; i++){
+        
+        if(takenNames[i]){
+            console.log("NAME TO RELEASE: "+nameToRelease.name);        
+            console.log("NAME WE'RE LOOKING AT: "+takenNames[i].name);
+            console.log("~~~~~~~~~~~~");
+            if(takenNames[i].name == nameToRelease.name){
+                console.log("TAKEN NAME: "+takenNames[i].name+" Name to release: "+nameToRelease.name);
+                delete takenNames[i];
+                nameWasFound = true;
+            }
+        }  
+    }//for
+}
+
+function validateName(thisMessage, nameToRelease){
+        /*First make sure the current username is safe,
+        because the user may have changed their name before
+        sending this message.*/
+    //let userIsChangingName = nameToRelease ? true : false;
+    let canUseName = isNameAvailable(thisMessage);
+
+    console.log("Is "+thisMessage.name+" available?: "+canUseName);
+    if (nameToRelease){
+        /*If there is a nameToRelease,
+        make that name available to the server.*/
+        makeNameAvailable(nameToRelease);             
+    }
+
+    return canUseName;
+}
 
 app.use(express.static(__dirname));
 app.use(bodyparser.json());
@@ -42,34 +96,62 @@ app.use(bodyparser.json());
 app.get('/messages', function(req, res){
     // console.log("GET CALLED");
     let messagesLength = messages.length - 1;
-    if (messagesLength < 0) messagesLength = 0;
+    if (messagesLength < 0){
+        //If there are no messages:
+        messagesLength = 0;
+    }
     // console.log(messagesLength);
-    res.send(messages[messagesLength]);
-});
-
-app.post('/messages', function(req, res){
-    req.body.message_id = messages.length;
-    messages.push(req.body);
-    console.log(messages.length + " mostrecent: " + req.body.message_data);
-
-    //messageSentEmitter.emit('message-sent', req.body);
-
+    //send the latest message.
+    //This bullshit will not work when the message
+    //id's are not sequential numbers
+    req.body.message = messages[messagesLength];
     res.send(req.body);
 });
 
+app.post('/messages', function(req, res){
+    
+    const thisMessage = req.body.message;
+    const nameToRelease = req.body.name;
+    // if(nameToRemove){
+    //     removeName(thisMessage);
+    // }
+    console.log("NEW MSG("+thisMessage.user_id+"): "+ thisMessage.message_data);
+    let canUseName = validateName(thisMessage, nameToRelease);
+    // let canUseName = checkIfNameInUse(thisMessage);    
+    console.log("Can use!!: " + canUseName);
+    console.log("===============")
+    if (canUseName){
+        //if the name is not taken already
+        /*This will stop you from using ANY name that
+        has already been taken. To make old names reusable,
+        we MAY need userIDs to be tracked, but we could also
+        just pass the old name along with the request perhaps*/
+
+        takenNames.push({name : thisMessage.name, id : thisMessage.user_id});
+        thisMessage.message_id = messages.length;
+        messages.push(thisMessage);
+        //console.log(messages.length + " mostrecent: " + req.body_data);
+        //console.log(thisMessage.user_id +" Sending: " + thisMessage.message_data);
+        // res.send(req.body);
+    } else {
+        //if the name is taken
+        req.body.message = {
+            "name": "Error",
+            "message_id": thisMessage.message_id,
+            "timestamp": thisMessage.timestamp,
+            "message_data": "Choose a new name! That one is already in use!",
+            "user_id" : thisMessage.user_id
+        };
+    }
+    res.send(req.body);
+});
+
+app.post('/taken-names', function(req, res){
+    res.send(nameWasFound);
+});
+
 app.put('/messages/:id', function(req, res){
-    //let id = req.params.id;
-    //let found = false;
-    let newName = req.body.newName;
-
-    products.forEach(function(product, index){
-        if(!found && product.id === Number(id)){
-            product.name = newName;
-        }
-    });
-
-    res.send('successfully updated products!');
-
+    res.send("This is a PUT request");
 });
 
 app.delete('/products/:id', function(req, res){
