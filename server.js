a8ff63cdb4160ed6115d4011cc588661c248f197
s3prototype <liuqahs15@gmail.com>
2020-11-15 23:29:01 -0500

    Added classes to the server.js. Still need to add a few more features, add classes to scripts, refactor, then we'll move on to adding the cool stuff.

--- server.js
+++ server.js
@@ -16,136 +16,153 @@ let defaultMessage = {
     "user_id" : "SERVER_MESSAGE"
 };
 
-let messages = [{}];//was: JSON.parse(rawData);
-let takenNames = [];
+// let messages = [{}];//was: JSON.parse(rawData);
+// let takenNames = [];
 
-userListID = 0;
+class NameContainer{
+    static #takenNames = [];
+    constructor(){ 
+        
+    }
 
-// let userList = [{}];
-// userList.hasChanged = false;
+    static initialize(){
+        NameContainer.#takenNames = [];
+    }
+
+    static addToTakenNames(thisMessage){
+        NameContainer.#takenNames.push({name : thisMessage.name, id : thisMessage.user_id});
+        //console.log("USER LIST ID: "+userListID);
+    }
+    static refreshTakenNames(){
+        NameContainer.#takenNames = NameContainer.#takenNames.filter(
+            function(name){
+                return name != null;
+            }
+        );
+        // NameContainer.#takenNames = refreshedNames;
+    }
+    static makeNameAvailable(nameToRelease){
+        if(!nameToRelease) return;
+        // let nameWasFound = false;
+
+        NameContainer.#takenNames = NameContainer.#takenNames.filter(
+            function(name){
+                return name && name.name != nameToRelease.name
+            }
+        );
+
+        // NameContainer.#takenNames = newList;
+    }
+    static isNameAvailable(thisMessage){
+        console.log("%%Is Name Available called%%");
+        let doesUserOwnIt = false;
+        let nameAvailable = true;
+
+        // let tempNames = NameContainer.#takenNames;
+        
+        for (let i = 0; nameAvailable == true && i < NameContainer.#takenNames.length; i++){
+            if (NameContainer.#takenNames[i] && NameContainer.#takenNames[i].name == thisMessage.name){
+                if (NameContainer.#takenNames[i].id == thisMessage.user_id){
+                    doesUserOwnIt = true;
+                    break;
+                } else {
+                    nameAvailable = false;
+                }//else
+            }//if (takenNames ...
+        }//for    
+        return {canUse : nameAvailable, alreadyOwnedByUser: doesUserOwnIt};
+    }
+    static validateName(thisMessage, nameToRelease){   
+        NameContainer.makeNameAvailable(nameToRelease);
+        const currentName = NameContainer.isNameAvailable(thisMessage);// returns an object
+        console.log('Is '+thisMessage.name+ ' available?: \
+        '+currentName.canUse+'| Already owned? '+currentName.alreadyOwnedByUser);
+
+        if(currentName.canUse){//was && nameToRelease
+            //If you're changing your name
+            // if(thisMessage.name.substr(0, 5) != 'ANON-'){
+            if(!currentName.alreadyOwnedByUser){
+                NameContainer.addToTakenNames(thisMessage);
+            }
+        }
+        
+        return currentName;
+    }
+    static getTakenNames(){
+        return NameContainer.#takenNames;
+    }
+}
+
+class UserListContainer{
+    constructor(){
+        
+    }
+    static #userListID;
+    static #userList = [];
+
+    static initialize(){
+        UserListContainer.#userList = [];
+    }
+    static generateNewListID(){
+        UserListContainer.#userListID = Math.floor(Math.random()* 10000);
+    }
+    static populateUserList(nameList){
+        UserListContainer.#userList = nameList.filter(name=>{
+            return name != null;
+        });
+    }
+    static getUserListID(){
+        return UserListContainer.#userListID;
+    }
+    static getUserList(){
+        return UserListContainer.#userList;
+    }
+}
+
+class MessageContainer{
+    constructor(){
+
+    }
+
+    static #messages = [];
+
+    static initialize(){
+        MessageContainer.#messages = [];
+    }
+    static generateNewMessageID(){
+        return Math.floor(Math.random()* 10000);        
+    }
+    static addMessageToDatabase(thisMessage){
+        thisMessage.message_id = MessageContainer.generateNewMessageID();
+        MessageContainer.#messages.push(thisMessage);
+    }
+    static isEmpty(){
+        return (MessageContainer.#messages.length <= 0)
+    }
+    static getMessageList(){
+        // const allMessages = MessageContainer.#messages;
+        return MessageContainer.#messages;
+    }
+}
 
 var port = process.env.PORT || 8090;
-
-function isNameAvailable(thisMessage){
-    console.log("%%Is Name Available called%%");
-    let doesUserOwnIt = false;
-    let nameAvailable = true;
-    for (i = 0; nameAvailable == true && i < takenNames.length; i++){
-        if (takenNames[i] && takenNames[i].name == thisMessage.name){
-            /*if the name is already taken, then check if the userID
-            matches. If so, then the name belongs to this user.*/
-            if (takenNames[i].id == thisMessage.user_id){
-                /*if the name is already in use, and it's
-                being used by this user, then we're done.*/
-                doesUserOwnIt = true;
-                break;
-            } else {
-                nameAvailable = false;
-            }//else
-        }//if (takenNames ...
-    }//for
-
-    return {canUse : nameAvailable, alreadyOwnedByUser: doesUserOwnIt};
-}
-
-function makeNameAvailable(nameToRelease){
-    if(!nameToRelease) return;
-    // let nameWasFound = false;
-
-    for(name of takenNames){
-        if(name && name.name == nameToRelease.name){
-            delete takenNames[i];
-        }
-    }
-
-    // for(i = 0; !nameWasFound && i < takenNames.length; i++){        
-    //     if(takenNames[i] && takenNames[i].name == nameToRelease.name){
-    //             console.log("RELEASING: "+takenNames[i].name+" BECAUSE IT MATCHED: "+nameToRelease.name);
-    //             delete takenNames[i];
-    //             nameWasFound = true;
-    //     }
-    // }
-}
-
-function addToTakenNames(thisMessage){
-    // userList.push({name : thisMessage.name, id : thisMessage.user_id});
-    // userList.hasChanged = true;
-    takenNames.push({name : thisMessage.name, id : thisMessage.user_id});
-    console.log("USER LIST ID: "+userListID);
-}
-
-function generateNewListID(){
-    userListID = Math.floor(Math.random()* 10000);
-}
-
-function generateNewMessageID(){
-    return Math.floor(Math.random()* 10000);
-}
-
-function validateName(thisMessage, nameToRelease){
-        /*First make sure the current username is safe,
-        because the user may have changed their name before
-        sending this message.*/
-        // console.log("BEFORE ANY CHECKS, THIS");
-        // console.log("IS THE NAME TO RELEASE: " + nameToRelease.name);
-        
-    // if(nameToRelease){
-        // console.log("MAKING NAME AVAILABLE: "+nameToRelease.name);
-        // if(nameToRelease.name.substr(0, 5) != 'ANON-'){
-            // console.log("MAKING NAME AVAILABLE: "+nameToRelease.name);
-            /*If it's not an anon-name.
-             Other users can't have the same anon name
-             anyway.*/
-            /*If there is a nameToRelease,
-            make that name available to the server.*/       
-            makeNameAvailable(nameToRelease);  
-        //  }      
-    // }
-        //let userIsChangingName = nameToRelease ? true : false;
-    const currentName = isNameAvailable(thisMessage);// returns an object
-    console.log('Is '+thisMessage.name+ ' available?: \
-    '+currentName.canUse);
-
-    if(currentName.canUse && !currentName.alreadyOwnedByUser){//was && nameToRelease
-        //If you're changing your name
-        //if(thisMessage.name.substr(0, 5) != 'ANON-'){
-            addToTakenNames(thisMessage);
-            generateNewListID();
-        //}
-    }
-    
-    return currentName.canUse;
-}
-
-function addMessageToDatabase(thisMessage){
-    thisMessage.message_id = generateNewMessageID();
-    messages.push(thisMessage);
-}
 
 app.use(express.static(__dirname));
 app.use(bodyparser.json());
 
 app.get('/messages', function(req, res){
     // console.log("GET CALLED");
-    let messagesLength = messages.length - 1;
-    req.body.message = messages.length > 0 ? messages[messages.length - 1] : messages[0];
-    if (messagesLength < 0){
-        //If there are no messages:
-        messagesLength = 0;
-    }
-    // console.log(messagesLength);
-    //send the latest message.
-    //This bullshit will not work when the message
-    //id's are not sequential numbers
-    // req.body.message = messages[messagesLength];
+    const messageList = MessageContainer.getMessageList()
+    const messageIndex = messageList.length > 0 ? messageList.length - 1 : 0;
+    req.body.message = messageList[messageIndex];
     res.send(req.body);
 });
 
 app.post('/initialize', function(req, res){
 
-    let nameValidated = false;
+    var nameValidated;
     if(req.body){
-        nameValidated = validateName(req.body, null);
+        nameValidated = NameContainer.validateName(req.body, null);
     }
 
     res.send("SUCCESS");
@@ -153,17 +170,22 @@ app.post('/initialize', function(req, res){
 
 app.get('/user-list', function(req, res){
     // let nameLength = takenNames.length;
-    let listToSend = [];
+    // let listToSend = [];
 
-    takenNames.forEach(function(name, index){
-        listToSend.push(name);
-    });
+    UserListContainer.populateUserList(NameContainer.getTakenNames());
+
+    // takenNames.forEach(function(name, index){
+    //     listToSend.push(name);
+    // });
 
     // for(i = 0; nameLength > 0 && i < nameLength; i++){        
     //     if(takenNames[i]){
     //     }
     // }
-    res.send({list : listToSend, listID : userListID});
+    res.send({
+        list : UserListContainer.getUserList(),
+        listID : UserListContainer.getUserListID()
+    });
 });
 
 app.post('/messages', function(req, res){
@@ -173,12 +195,15 @@ app.post('/messages', function(req, res){
     // if(nameToRemove){
     //     removeName(thisMessage);
     // }
-    console.log("NEW MSG("+thisMessage.user_id+")["+ thisMessage.name+"]: "+ thisMessage.message_data);
-    const canUseName = validateName(thisMessage, nameToRelease);
+    // console.log("NEW MSG("+thisMessage.user_id+")["+ thisMessage.name+"]: "+ thisMessage.message_data);
+    // const nameResult = validateName(thisMessage, nameToRelease);
+    const nameResult = NameContainer.validateName(thisMessage, nameToRelease);
     // let canUseName = checkIfNameInUse(thisMessage);    
-    console.log("Can use!!: " + canUseName);
-    console.log("===============")
-    if (canUseName){
+    // console.log("Can use!!: " + canUseName);
+    // console.log("===============")
+    if (nameResult.canUse){
+        UserListContainer.generateNewListID();
+        MessageContainer.addMessageToDatabase(thisMessage);
         //if the name is not taken already
         /*This will stop you from using ANY name that
         has already been taken. To make old names reusable,
@@ -188,7 +213,6 @@ app.post('/messages', function(req, res){
         //takenNames.push({name : thisMessage.name, id : thisMessage.user_id});
         // thisMessage.message_id = generateNewMessageID();
         // messages.push(thisMessage);
-        addMessageToDatabase(thisMessage);
         //console.log(messages.length + " mostrecent: " + req.body_data);
         //console.log(thisMessage.user_id +" Sending: " + thisMessage.message_data);
         // res.send(req.body);
@@ -222,12 +246,14 @@ app.delete('/messages', function(req, res){
         return;
     }
 
-    makeNameAvailable(nameToRelease);
-    generateNewListID();
+    NameContainer.makeNameAvailable(nameToRelease);
+    // makeNameAvailable(nameToRelease);
+    UserListContainer.generateNewListID();
     res.send("Delete succeeded.");
 });
 
 app.listen(port, function(){
-    // console.log(`Server listening on: ${port}`);
-    // console.log(messages[0].message_data);
+    console.log(`Server listening on: ${port}`);
+    console.log("Server start date/time: "+new Date().toLocaleDateString() + " / " + new Date().toLocaleTimeString());
+    console.log("======");
 });
