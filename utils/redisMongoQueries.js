294f683360abf20bd886c5d75387bf75a2ad430f
S3Prototype <liuqahs15@gmail.com>
2021-02-07 14:00:21 -0500

    Uh huh

--- utils/redisMongoQueries.js
+++ utils/redisMongoQueries.js
@@ -517,7 +517,7 @@ function getRedisClient(){
 function createRedisClient(){
     redisClient = redis.createClient({
         port      : 6379,               // replace with your port
-        host      : '120.0.0.1',        // replace with your hostanme or IP address
+        host      : 'https://practical-elion-41f9cb.netlify.app/',        // replace with your hostanme or IP address
         password  : 'your password'    // replace with your password
       });
     getRoomFromRedis = promisify(redisClient.hgetall).bind(redisClient);
